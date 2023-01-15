f2158dfaa3ff458e93841f3dc62cdc63752ab077
Vicky Shen <vicshe01@gmail.com>
2023-01-15 10:12:50 -0500

    commit tree 0e62e72ff76009c9227be18be30f0a95da2d4f83

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+destructivelyAppendCat(Ralph);
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const newCats = [...cats]  
+    //newCats = ['Milo', 'Otis', 'Garfield']
+    newCats.push(name)
+    //newCats = ['Milo', 'Otis', 'Garfield', name]
+    return newCats;
+}
+
+function prependCat(name) {
+    const brandNewCats = [...cats]
+    brandNewCats.unshift(name)
+    return brandNewCats;
+}
+
+function removeLastCat() {
+    const catsRemoved = [...cats]
+    catsRemoved.pop()
+    return catsRemoved;
+}
+
+function removeFirstCat() {
+    const moreCatsRemoved = [...cats]
+    moreCatsRemoved.shift()
+    return moreCatsRemoved;
+}
